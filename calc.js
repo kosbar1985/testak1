f6b7b357591877b9fd49999d5776e5a8eac60500
kosbar1985 <49440560+kosbar1985@users.noreply.github.com>
2020-03-16 13:50:53 +0200

    fix bugs, beautify code

--- calc.js
+++ calc.js
@@ -1,23 +1,33 @@
-var data_mod = [];
-let count_pos=0;
-let count_neg=0;
-let count_zero=0;
-let count_null=0;
-for(let i=0;i<data.length;i++){
-    if (typeof data[i]=="number"&&data[i] != 0){
-    data_mod[i] = data[i]+1;
-    if (data_mod[i]>0){count_pos++;} else {count_neg++}
+let data_mod = [];
+let count_pos = 0;
+let count_neg = 0;
+let count_zero = 0;
+let count_null = 0;
 
-    } else if
-        (typeof data[i] =="string"&&!isNaN(Number(data[i]))&&data[i] != 0){
-        data_mod[i]=Number(data[i]);
-        data_mod[i]++;
-        data_mod[i]=String(data_mod[i]);
-
-        } else{
-      if (data[i] == 0&&typeof data[i]=='number') {count_zero++;}
-      if (Object.is(data[i], null)) {count_null++;} 
-         data_mod[i]=String(data [i]);
-        
+for (let i = 0; i < data.length; i++) {
+    if (typeof data[i] == "number" && data[i] != 0) {
+        data_mod[i] = data[i] + 1;
+        if (data_mod[i] > 0) {
+            count_pos++;
+        } else {
+            count_neg++
         }
+
+    } else if (typeof data[i] == "string" && !isNaN(Number(data[i])) && data[i] != 0) {
+        data_mod[i] = Number(data[i]);
+        data_mod[i]++;
+        data_mod[i] = String(data_mod[i]);
+
+    } else {
+        if (data[i] == 0 && typeof data[i] == 'number') {
+            count_zero++;
+        }
+        if (Object.is(data[i], null)) {
+            count_null++;
+        }
+        data_mod[i] = data[i];
+
     }
+
+}
+alert(data_mod);
--- log.js
+++ log.js
@@ -1,10 +1,10 @@
-document.querySelectorAll('span')[0].innerHTML = data_mod.length;
+document.querySelectorAll('.total-count > span')[0].innerHTML = data_mod.length;
 console.log(data_mod.length);
-document.querySelectorAll('span')[1].innerHTML = count_pos;
+document.querySelectorAll('.more-count > span')[0].innerHTML = count_pos;
 console.log(count_pos);
-document.querySelectorAll('span')[2].innerHTML = count_neg;
+document.querySelectorAll('.less-count > span')[0].innerHTML = count_neg;
 console.log(count_neg);
-document.querySelectorAll('span')[3].innerHTML = count_zero;
+document.querySelectorAll('.equal-count > span')[0].innerHTML = count_zero;
 console.log(count_zero);
-document.querySelectorAll('span')[4].innerHTML = count_null;
+document.querySelectorAll('.null-count > span')[0].innerHTML = count_null;
 console.log(count_null);
